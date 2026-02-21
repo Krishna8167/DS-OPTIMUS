d63e9914751bbd1b0d483661a2f8e4ba853d341b
Krishna Sharma <156648921+Krishna8167@users.noreply.github.com>
2026-02-21 14:11:18 +0530

    Update 129_Sum_root_to_leaf_Numbers.java
    more solution

--- 129_Sum_root_to_leaf_Numbers.java
+++ 129_Sum_root_to_leaf_Numbers.java
@@ -34,3 +34,43 @@ class Solution {
         return sum;
     }
 }
+
+
+// solution 2: without backtracking
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int sum = 0;
+    StringBuilder sb = new StringBuilder();
+
+    public int sumNumbers(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        sb.append(root.val);
+
+        if (root.left == null && root.right == null) {           
+            sum += Integer.parseInt(sb.toString());
+        } else {
+            sumNumbers(root.left);
+            sumNumbers(root.right);
+        }
+
+        sb.deleteCharAt(sb.length() - 1);
+        return sum;
+    }
+}
